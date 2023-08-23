e6021a5978d008b537a191c2c504822f44a983d7
wenyu.ywm <yangweiming.ywm@alibaba-inc.com>
2023-08-23 16:18:16 +0800

    fix: add example sql with hints

---
+++ examples/sql/create_table_with_hints/main.go
@@ -0,0 +1,17 @@
+package main
+
+func main() {
+	//config, err := sqldriver.NewConfigFromIni(os.Args[1])
+	//if err != nil {
+	//	log.Fatalf("%+v", err)
+	//}
+	//
+	//dsn := config.FormatDsn()
+	//// or dsn := "http://<accessId>:<accessKey>@<endpoint>?project=<project>"
+	//
+	//_, err = sql.Open("odps", dsn)
+	//if err != nil {
+	//	log.Fatalf("%+v", err)
+	//}
+
+}
