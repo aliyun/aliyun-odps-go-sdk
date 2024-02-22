df4674383eb63555a36eba1bf62a58ac5b60bf7e
舍然 <liukaizeng.lkz@alibaba-inc.com>
2024-02-22 16:20:49 +0800

    chord: 添加注释

--- odps/tunnel/common.go
+++ odps/tunnel/common.go
@@ -127,6 +127,13 @@ func (bw *bufWriter) Close() error {
 	return nil
 }
 
+// 用于记录tunnel上传时http发送的数据量大小。对于压缩后的数据不能通过
+// write方法返回的n来确定压缩后的数据大小。只能
+// 1. 用bytesRecordWriter包装http conn
+// 2. 用compressor writer包装bytesRecordWriter
+// 3. compressor在压缩数据后，会调用bytesRecordWriter.write
+// 4. bytesRecordWriter.write接收到的数据就是经过compressor压缩后的数据
+// 5. bytesRecordWriter.write记录接收到的数据大小
 type bytesRecordWriter struct {
 	writer io.WriteCloser
 	bytesN int
