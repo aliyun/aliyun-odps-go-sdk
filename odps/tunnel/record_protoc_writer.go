7bc88325d8552d4cd92ae439ce22200de45d8aea
舍然 <liukaizeng.lkz@alibaba-inc.com>
2024-03-25 11:27:30 +0800

    feat: tunnel record protoc writer在写数据过程中，如果http连接中断，会造成协程泄露

--- odps/tunnel/record_protoc_writer.go
+++ odps/tunnel/record_protoc_writer.go
@@ -64,6 +64,20 @@ func newRecordProtocHttpWriter(conn *httpConnection, columns []tableschema.Colum
 }
 
 func (r *RecordProtocWriter) Write(record data.Record) error {
+	err := r.write(record)
+
+	if err != nil {
+		err1 := r.Close()
+
+		if err1 != nil {
+			return errors.WithStack(err1)
+		}
+	}
+
+	return errors.WithStack(err)
+}
+
+func (r *RecordProtocWriter) write(record data.Record) error {
 	// 这里加一个判断会引起不必要的耗时
 	//if r.closed {
 	//	return errors.New("cannot write to a closed RecordProtocWriter")
