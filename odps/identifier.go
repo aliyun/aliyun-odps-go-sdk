170e2c162f2eb15c80ff66144e606d383e63210b
鼎昕 <zhangdingxin.zdx@alibaba-inc.com>
2024-10-29 13:50:23 +0800

    doc: add doc of TableIdentifier

--- odps/identifier.go
+++ odps/identifier.go
@@ -1,11 +1,15 @@
 package odps
 
+// TableIdentifier is used to uniquely identify a table in MaxCompute, compatible with both the three-layer model and the two-layer model.
 type TableIdentifier struct {
 	ProjectName string
 	SchemaName  string
 	TableName   string
 }
 
+// String returns the string representation of a TableIdentifier.
+// For users with the Schema model enabled, the format is "ProjectName.SchemaName.TableName".
+// For users who do not have it enabled, the format is "ProjectName.TableName".
 func (t *TableIdentifier) String() string {
 	if t.ProjectName == "" {
 		return t.TableName
@@ -16,6 +20,12 @@ func (t *TableIdentifier) String() string {
 	return t.ProjectName + "." + t.SchemaName + "." + t.TableName
 }
 
+// TableId generates a TableIdentifier instance based on the provided parameters.
+// This function supports the following number of parameters:
+// - 3 parameters: returns a TableIdentifier containing ProjectName, SchemaName, and TableName.
+// - 2 parameters: returns a TableIdentifier containing ProjectName and TableName, with SchemaName empty.
+// - 1 parameter: returns a TableIdentifier containing only TableName, with ProjectName and SchemaName empty.
+// - 0 parameters: returns an empty TableIdentifier instance.
 func TableId(option ...string) *TableIdentifier {
 	if len(option) >= 3 {
 		return &TableIdentifier{
