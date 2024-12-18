2ab5df49bf946dc87a9c9409b18ce3e985bd8c17
robin1 <haochuan.lj@alibaba-inc.com>
2024-12-18 16:13:41 +0800

    Feature: 支持 Function 相关的 CRUD 操作 (#56)
    Co-authored-by: Jason Zhang <50667759+dingxin-tech@users.noreply.github.com>

---
+++ examples/sdk/function/create_function/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"log"
+
+	"github.com/aliyun/aliyun-odps-go-sdk/odps"
+	"github.com/aliyun/aliyun-odps-go-sdk/odps/account"
+)
+
+func main(){
+	// Specify the ini file path
+	configPath := "./config.ini"
+	conf, err := odps.NewConfigFromIni(configPath)
+	if err != nil {
+		log.Fatalf("%+v", err)
+	}
+
+	aliAccount := account.NewAliyunAccount(conf.AccessId, conf.AccessKey)
+	odpsIns := odps.NewOdps(aliAccount, conf.Endpoint)
+	// Set the Default Maxcompute project used By Odps instance
+	odpsIns.SetDefaultProjectName(conf.ProjectName)
+	odpsIns.SetCurrentSchemaName("default")
+
+	fb := odps.NewFunctionBuilder()
+	fb.Name("test_sleep2")
+	fb.ClassPath("org.example.Sleep")
+	fb.Resources([]string{"udf-1.0-SNAPSHOT.jar"})
+	f := fb.Build()
+
+	functions := odps.NewFunctions(odpsIns)
+	err = functions.Create("", "", f)
+	if err != nil {
+		panic(err)
+	}
+}
