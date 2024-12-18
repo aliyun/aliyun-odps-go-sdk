09f187de931cab439d5dd84d9fb1050f5c1e8b64
robin1 <haochuan.lj@alibaba-inc.com>
2024-12-18 16:11:10 +0800

    Feature: 支持创建、更新、删除资源 (#55)
    add comment for resource builder

---
+++ examples/sdk/resources/list_resources/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/aliyun/aliyun-odps-go-sdk/odps"
+	"github.com/aliyun/aliyun-odps-go-sdk/odps/account"
+)
+
+func main(){
+	// Specify the ini file path
+	configPath := "./config.ini"
+	conf, err := odps.NewConfigFromIni(configPath)
+	if err != nil {
+		log.Fatalf("%+v", err)
+	}
+
+	aliAccount := account.NewAliyunAccount(conf.AccessId, conf.AccessKey)
+	odpsIns := odps.NewOdps(aliAccount, conf.Endpoint)
+	// Set the Default Maxcompute project used By Odps instance
+	odpsIns.SetDefaultProjectName(conf.ProjectName)
+	odpsIns.SetCurrentSchemaName("default")
+	resources := odps.NewResources(odpsIns)
+
+	resources.List(func(resource *odps.Resource, err error) {
+		if err != nil {
+			log.Fatalf("%+v", err)
+		}
+		fmt.Printf("%+v\n", resource)
+	})
+
+	resource1:= resources.Get("bank_customer.txt")
+	fmt.Println(resource1.Exist())
+}
---
+++ examples/sdk/resources/update_resource/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/aliyun/aliyun-odps-go-sdk/odps"
+	"github.com/aliyun/aliyun-odps-go-sdk/odps/account"
+)
+
+func main(){
+	// Specify the ini file path
+	configPath := "./config.ini"
+	conf, err := odps.NewConfigFromIni(configPath)
+	if err != nil {
+		log.Fatalf("%+v", err)
+	}
+
+	aliAccount := account.NewAliyunAccount(conf.AccessId, conf.AccessKey)
+	odpsIns := odps.NewOdps(aliAccount, conf.Endpoint)
+	// Set the Default Maxcompute project used By Odps instance
+	odpsIns.SetDefaultProjectName(conf.ProjectName)
+	odpsIns.SetCurrentSchemaName("default")
+	resources := odps.NewResources(odpsIns)
+
+	file,err:=os.Open("xxxx")
+	if err!=nil{
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	fr := odps.NewFileResource("bank_data")
+	fr.SetReader(file)
+
+	err=resources.UpdateFileResource("","",fr)
+	if err!=nil{
+		log.Fatal(err)
+	}
+}
