63443051b1b65d98da545804ac32c0dd4b5ce18e
鼎昕 <zhangdingxin.zdx@alibaba-inc.com>
2025-06-09 10:11:32 +0800

    test(odps/tables_test.go): adds test cases for batch loading tables

--- odps/tables_test.go
+++ odps/tables_test.go
@@ -15,3 +15,19 @@ func TestTables_List(t *testing.T) {
 		print(table.Name() + "\t" + table.Type().String() + "\n")
 	})
 }
+
+func TestTables_BatchLoadTables(t *testing.T) {
+	var tables []string
+
+	odpsIns.DefaultProject().Schemas().Get("mv").Tables().List(func(table *odps.Table, err error) {
+		tables = append(tables, table.Name())
+	})
+
+	loadTables, err := odpsIns.Schemas().Get("mv").Tables().BatchLoadTables(tables)
+	if err != nil {
+		t.Error(err)
+	}
+	for _, table := range loadTables {
+		print(table.Name() + "\t" + table.Type().String() + "\n")
+	}
+}
